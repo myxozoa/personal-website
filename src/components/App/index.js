81a855ebb7519a39b0fcec5e5af1de4187fe1462
Ronnie Miksch <fakepreferences@gmail.com>
2018-10-21 15:41:59 -0700

    Placeholder navlinks

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,9 +4,15 @@ import styles from './App.module.css';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className={styles.app}>
         <header className={styles.header}>
           <span>Ronnie Miksch</span>
+
+          <nav>
+            <div>Home</div>
+            <div>Projects</div>
+            <div>Contact</div>
+          </nav>
         </header>
       </div>
     );
