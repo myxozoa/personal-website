7d91e75baf39ab3f7140ef1673abe7084c5e8d1a
Ronnie Miksch <fakepreferences@gmail.com>
2018-10-21 15:34:35 -0700

    Creating file structure and css modules

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './App.module.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className={styles.header}>
+          <span>Ronnie Miksch</span>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
