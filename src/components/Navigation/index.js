1881f6dbb63e3a81bec628349d8e9923bbc3bdae
Ronnie Miksch <fakepreferences@gmail.com>
2018-10-22 00:41:40 -0700

    Populating projects from json file and functional project links

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -7,9 +7,9 @@ class Navigation extends React.Component {
   render() {
     return (
       <nav className={styles.container}>
-        <Link className="glitch" data-text="Home" style={{ '--offset': `${Math.random() * 3}s` }} to="/">Home</Link>
-        <Link className="glitch" data-text="Projects" style={{ '--offset': `${Math.random() * 2}s` }} to="/projects">Projects</Link>
-        <Link className="glitch" data-text="Contact" style={{ '--offset': `${Math.random() * 5}s` }} to="/contact">Contact</Link>
+        <Link className={['navLink', 'glitch'].join(' ')} data-text="Home" style={{ '--offset': `${Math.random() * 3}s` }} to="/">Home</Link>
+        <Link className={['navLink', 'glitch'].join(' ')} data-text="Projects" style={{ '--offset': `${Math.random() * 2}s` }} to="/projects">Projects</Link>
+        <Link className={['navLink', 'glitch'].join(' ')} data-text="Contact" style={{ '--offset': `${Math.random() * 5}s` }} to="/contact">Contact</Link>
       </nav>
     );
   }
--- src/components/Pages/Projects/index.js
+++ src/components/Pages/Projects/index.js
@@ -3,18 +3,26 @@ import Tilt from 'react-tilt'
 
 import styles from './Projects.module.css';
 
+import projects from '../../../assets/projects.json';
+
 class Projects extends React.Component {
   render() {
     return (
       <div className={styles.container}>
-        <h2 className="glitch" data-text="Projects">Projects</h2>
-        <section>
-        <Tilt className="Tilt" options={{ max : 10, reverse: true, scale: 1.01 }} style={{ height: 250, width: 250 }} >
-          <div className={styles.project}>
-            <div className={styles.projectName}>Main Course</div>
-            <div className={styles.projectDesc}>Restaurant App for the ages broham</div>
-          </div>
-        </Tilt>
+        <h2 className={[styles.title, 'glitch'].join(' ')} data-text="Projects">Projects</h2>
+        <section className={styles.projects}>
+        {
+          projects.map(project => (
+          <Tilt key={Math.random()} className="Tilt" options={{ max : 10, reverse: true, scale: 1.01 }} style={{ height: 250, width: 250 }}>
+            <a href={project.url} className={styles.link}>
+              <div className={styles.project}>
+                <div className={styles.projectName}>{project.title}</div>
+                <div className={styles.projectDesc}>{project.description}</div>
+              </div>
+            </a>
+          </Tilt>
+          ))
+        }
         </section>
       </div>
     );
