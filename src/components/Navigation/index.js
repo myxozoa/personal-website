f72e73563908e426a23d71ed72493a40be422a6c
Ronnie Miksch <fakepreferences@gmail.com>
2018-10-21 19:34:23 -0700

    Glitch text

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -7,9 +7,9 @@ class Navigation extends React.Component {
   render() {
     return (
       <nav className={styles.container}>
-        <Link to="/">Home</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/contact">Contact</Link>
+        <Link className="glitch" data-text="Home" style={{ '--offset': `${Math.random() * 3}s` }} to="/">Home</Link>
+        <Link className="glitch" data-text="Projects" style={{ '--offset': `${Math.random() * 2}s` }} to="/projects">Projects</Link>
+        <Link className="glitch" data-text="Contact" style={{ '--offset': `${Math.random() * 5}s` }} to="/contact">Contact</Link>
       </nav>
     );
   }
