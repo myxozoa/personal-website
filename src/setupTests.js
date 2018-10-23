e6ed7ea9363ca1c06766cb9fc7bd59df87437686
Ronnie Miksch <fakepreferences@gmail.com>
2018-10-23 16:11:04 -0700

    changed test math.random to a jest mock test

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,5 +5,5 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 const mockMath = Object.create(global.Math);
-mockMath.random = () => 0.5;
+mockMath.random = jest.fn(() => 0.5);
 global.Math = mockMath;
